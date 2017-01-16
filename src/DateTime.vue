d5fb1f3c76167548f3c8bbff324ac23df7cf39ae
Bryan Kneis <bryan@iqwebcreations.com>
2017-01-16 12:32:48 +0000

    Use jquery to select only the compoent's input element, instead of using generic class selector

--- src/DateTime.vue
+++ src/DateTime.vue
@@ -18,11 +18,22 @@ export default {
     name : 'date-time',
     props : { title : {}, options : {}, value : { default : '' } },
     mounted() {
-        $('.date').datetimepicker(this.options)
-             .on('dp-change', (val) => {
+        /**
+        The jquery selector gets the root element the vue component is attached to,
+        then filters down to the first text input, which in this template, will always
+        be the datetime input.
+
+        It then initialises it with any options passed through the options prop,
+        emits an input event when the date input changes, allowing the component to
+        be bound to vue reactivity system via v-model. Lastly, any initial value passed
+        through the props will initialise the start value for the input.
+        */
+        $(this.$el).children(':text').first()
+            .datetimepicker(this.options)
+            .on('dp-change', (val) => {
                 this.$emit('input', val);
-             });
-        $('.date').value(this.value);
+            })
+            .value(this.value);
     }
 }
 </script>
