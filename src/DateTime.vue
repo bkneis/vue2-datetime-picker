cf36274adbb20203386ad76d17f36c43c9970a7d
Bryan Kneis <bryan@iqwebcreations.com>
2017-01-16 11:10:00 +0000

    Add Datetime component and index file to export as js for projects importanting the library without vue-loader

---
+++ src/DateTime.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class='col-sm-6'>
+        <div class="form-group">
+            <div class='input-group date'>
+                <input type='text' class="form-control" />
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"/>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import jQuery from 'jquery';
+import 'eonasdan-bootstrap-datetimepicker';
+export default {
+    name : 'date-time',
+    props : { title : {}, options : {}, value : { default : '' } },
+    mounted() {
+        $('.date').datetimepicker(this.options)
+             .on('dp-change', (val) => {
+                this.$emit('input', val);
+             });
+        $('.date').value(this.value);
+    }
+}
+</script>
+<style scope>
+@import '../node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css';
+</style>
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import DateTime from './DateTime.vue'
+
+export {
+    DateTime
+}
